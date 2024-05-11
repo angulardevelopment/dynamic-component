3de7e8b849f4e2b8ca46a7f842fc3092ed5915c6
Gupta, Sunny <sunnyvales789@gmail.com>
2024-05-11 18:17:17 +0530

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,46 @@
-import { Component, ContentChildren, forwardRef, Inject, Injectable, QueryList, ViewChild, } from '@angular/core';
+import {
+  Component,
+  ContentChildren,
+  forwardRef,
+  Inject,
+  Injectable,
+  QueryList,
+  ViewChild,
+} from '@angular/core';
 import { TestDirective } from './demo3/app/test.directive';
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'mini-projects';
-@ViewChild(TestDirective) dir: TestDirective;
-changeCol()
-{
-  this.dir.changeColor('blue');
+  @ViewChild(TestDirective) dir: TestDirective;
+  changeCol() {
+    this.dir.changeColor('blue');
+  }
 }
-}
-
 
 @Component({
   selector: 'child',
-  template: '<div><h1>Child</h1></div>'
+  template: '<div><h1>Child</h1></div>',
 })
-export class Child {
-}
+export class Child {}
 
 @Component({
   selector: 'parent',
-  template: '<div><h1>Parent</h1><ng-content></ng-content></div>'
+  template: '<div><h1>Parent</h1><ng-content></ng-content></div>',
 })
 export class Parent {
   @ContentChildren(Child) children: QueryList<Child>;
   // @ContentChildren(forwardRef(() => Child)) children; // <!- HERE
   // @ContentChildren(Child, { descendants: true }) children: QueryList<Child>;
 
-
   ngAfterContentInit() {
-      console.log(this.children);
-      console.log(this.children.toArray());
+    console.log(this.children);
+    console.log(this.children.toArray());
 
-      console.log(this.children.length);
-
+    console.log(this.children.length);
   }
 }
